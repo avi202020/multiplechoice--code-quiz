5d21777cbf3407e3f649cf161a20ec8bbb9cd9d6
avi202020 <avadhesh.cs@gmail.com>
2023-03-03 18:26:14 +0000

    javascript script created to access tags from html also samle questions file created

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+// Add all variables (selectors) to link to the HTML
+var questionsEL = document.querySelector("#questions");
+var timer = document.querySelector("#time");
+var choicesEL = document.querySelector("#choices");
+var submitButton = document.querySelector("#submit");
+var startButton = document.querySelector("#start");
+var initialsEl = document.querySelector("#initials");
+var feedback = document.querySelector("#feedback");
+var clearScoreBtn = document.getElementById("clear");
